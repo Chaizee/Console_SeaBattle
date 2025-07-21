d2e223345b5d4abdffd35a6c24c98c728895c3e8
Dmitriy <zuzindima26@gmail.com>
2025-07-21 21:41:47 +0600

    add seaBattle

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Security.Authentication;
+
+namespace sea_battle
+{
+    class Cell
+    {
+        public bool hasShip;
+        public bool wasFired;
+        public bool canPlaceShip;
+
+        public Cell(bool hasship, bool wasfired, bool canplaceship)
+        {
+            hasShip = hasship;
+            wasFired = wasfired;
+            canPlaceShip = canplaceship;
+        }
+    }
+
+    class Program
+    {   
+        public static List<Cell> MadeField()
+        {
+            List<Cell> field = new List<Cell>();
+            return field;
+        }
+
+        public static void PrintField(List<Cell> field)
+        {
+            Console.WriteLine("  0 1 2 3 4 5 6 7 8 9");
+            for (int i = 0; i <= 9; i++)
+            {
+                Console.WriteLine($"{i} * * * * * * * * * *");
+            }
+        }
+
+
+        public static void Main()
+        {
+            Console.WriteLine("Приветствуем вас в морском бое!");
+
+            Console.WriteLine("1. Играть");
+            Console.WriteLine("2. Выход");
+            
+            Console.Write("\nВыберите действие: ");
+            var firstInputLine = Console.ReadLine();
+            int firstChoice;
+            if (!int.TryParse(firstInputLine, out firstChoice)) {
+                Console.WriteLine("Неверный выбор, попробуйте снова!");
+            }
+            Console.Clear();
+            switch(firstChoice)
+            {
+                case 1:
+                    Console.WriteLine("Выберите режим: ");
+                    Console.WriteLine("1. Играть с другом");
+                    Console.WriteLine("2. Играть с пк");
+                    Console.WriteLine("3. Выход");
+
+                    var secondInputLine = Console.ReadLine();
+                    int secondChoice;
+                    if (!int.TryParse(secondInputLine, out secondChoice))
+                    {
+                        Console.WriteLine("Неверный выбор, попробуйте снова!");
+                    }
+
+                    switch(secondChoice)
+                    {
+                        case 1:
+                            Console.WriteLine("Приятной игры!\n");
+                            List<Cell> firstField = MadeField();
+                            PrintField(firstField);
+                            Console.WriteLine("\nИгрок 1, расставьте корабли:");
+                            
+                            return;
+                        case 2:
+                            return;
+                        case 3:
+                            break;
+                        default:
+                            Console.WriteLine("Неверный выбор, попробуйте снова!");
+                            break;
+
+                    }
+
+                    break;
+                case 2:
+                    Console.WriteLine("Выбрано действие 2");
+                    break;
+                default:
+                    Console.WriteLine("Неверный выбор, попробуйте снова!");
+                    break;
+
+            }
+        }
+    }
+}
